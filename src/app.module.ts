fa7c8d89c29777880cf99c93b1263b7f8ba4c589
woosang0430 <yws1502@naver.com>
2022-12-19 22:50:58 +0900

    ✨ typeorm 데이터베이스 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const typeOrmModuleOptions = {
+  useFactory: async (): Promise<TypeOrmModuleOptions> => {
+    return {
+      namingStrategy: new SnakeNamingStrategy(),
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true, // set 'false' in production
+      autoLoadEntities: true,
+      logging: true, // production에서는 false로
+      keepConnectionAlive: true,
+    };
+  },
+};
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
