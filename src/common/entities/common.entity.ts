ec26c4edabdf6af043fe5a08fc026d49382a796b
woosang0430 <yws1502@naver.com>
2023-01-24 22:15:02 +0900

    🧑‍💻 #1 - 공용 entity 생성

---
+++ src/common/entities/common.entity.ts
@@ -0,0 +1,31 @@
+import { Exclude } from 'class-transformer';
+import { IsUUID } from 'class-validator';
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class CommonEntity {
+  @IsUUID()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  // Postgres의 time zone이 'UTC'리먄 UTC 기분으로 출력하고 'Asia/Seoul'이라면 서울 기준으로 출력한다.
+  // DB SQL QUERY : set time zone 'Asia/Seoul'; show timezone;
+  @CreateDateColumn({
+    type: 'timestamptz' /* timestamp with time zone */,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz' /* timestamp with time zone */,
+  })
+  updateddAt: Date;
+
+  // Soft Delete : 기존에는 null, 삭제 시 timestamp를 찍는다.
+  @Exclude()
+  @DeleteDateColumn({ type: 'timestamptz' })
+  deleteAt?: Date | null;
+}
