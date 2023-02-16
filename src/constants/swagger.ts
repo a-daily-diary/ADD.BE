87bf2ce223cbd413e1bce2e6b2234879502497bb
yws1502 <yws1502@naver.com>
2023-02-16 20:49:51 +0900

    🧑‍💻 #3 - diary api swagger 문서 적용

--- src/constants/swagger.ts
+++ src/constants/swagger.ts
@@ -1,97 +1,142 @@
+const responseTemplate = (data: any) => {
+  return {
+    schema: {
+      example: {
+        success: true,
+        data,
+      },
+    },
+  };
+};
+
 export const responseExample = {
-  uploadUserImg: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          imgUrl: 'url image path',
+  // USER
+  uploadUserImg: responseTemplate({
+    imgUrl: 'url image path',
+  }),
+  getCurrentUser: responseTemplate({
+    id: 'uuid',
+    createdAt: 'Data string',
+    updateddAt: 'Data string',
+    email: 'email string',
+    username: 'username string',
+    thumbnailUrl: 'url image path',
+    isAgree: true,
+    isAdmin: false,
+  }),
+  getUserInfo: responseTemplate({
+    id: 'uuid',
+    createdAt: 'Data string',
+    updateddAt: 'Data string',
+    email: 'email string',
+    username: 'username string',
+    thumbnailUrl: 'url image path',
+    isAgree: true,
+    isAdmin: false,
+  }),
+  join: responseTemplate({
+    message: '회원가입에 성공하였습니다.',
+  }),
+  emailCheck: responseTemplate({
+    message: '사용가능한 이메일입니다.',
+  }),
+  usernameCheck: responseTemplate({
+    message: '사용가능한 유저이름입니다.',
+  }),
+  login: responseTemplate({
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0YWY4OGI2OC0yNzc4LTQ1NjEtOTMzZS0zZDg2YTFmNmJhODciLCJpYXQiOjE2NzQ5NzQzNzQsImV4cCI6MTY3NTA2MDc3NH0.7qoTypzUQTfIULMoKPLYpXOcpTVtQZcsMKjdSnHRT2U',
+    user: {
+      id: 'uuid',
+      createdAt: 'Data string',
+      updateddAt: 'Data string',
+      email: 'email string',
+      username: 'username string',
+      thumbnailUrl: 'url image path',
+      isAgree: true,
+      isAdmin: false,
+    },
+  }),
+
+  // DIARY
+  getDiaries: responseTemplate([
+    {
+      token: 'tokenValue',
+      user: {
+        id: '02797c95-179b-4dfc-9f91-29d8a4292859',
+        createdAt: '2023-02-08T15:17:31.103Z',
+        updateddAt: '2023-02-08T15:17:31.103Z',
+        title: 'test',
+        content: 'etsf',
+        imgUrl: null,
+        favoriteCount: 0,
+        commentCount: 0,
+        author: {
+          id: 'b63d1c3f-9308-4fe4-b9f7-b80c504f3699',
+          createdAt: '2023-02-08T11:58:52.163Z',
+          updateddAt: '2023-02-08T11:58:52.163Z',
+          email: 'woosang@test.com',
+          username: 'yws',
+          thumbnailUrl: 'http://127.0.0.1:5000',
         },
       },
     },
-  },
-  getCurrentUser: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          id: 'uuid',
-          createdAt: 'Data string',
-          updateddAt: 'Data string',
-          email: 'email string',
-          username: 'username string',
-          thumbnailUrl: 'url image path',
-          isAgree: true,
-          isAdmin: false,
-        },
-      },
+  ]),
+  getDiary: responseTemplate({
+    id: '02797c95-179b-4dfc-9f91-29d8a4292859',
+    createdAt: '2023-02-08T15:17:31.103Z',
+    updateddAt: '2023-02-08T15:17:31.103Z',
+    title: 'test',
+    content: 'etsf',
+    imgUrl: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    author: {
+      id: 'b63d1c3f-9308-4fe4-b9f7-b80c504f3699',
+      createdAt: '2023-02-08T11:58:52.163Z',
+      updateddAt: '2023-02-08T11:58:52.163Z',
+      email: 'woosang@test.com',
+      username: 'yws',
+      thumbnailUrl: 'http://127.0.0.1:5000',
     },
-  },
-  getUserInfo: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          id: 'uuid',
-          createdAt: 'Data string',
-          updateddAt: 'Data string',
-          email: 'email string',
-          username: 'username string',
-          thumbnailUrl: 'url image path',
-          isAgree: true,
-          isAdmin: false,
-        },
-      },
+  }),
+  createDiary: responseTemplate({
+    title: 'qweqwe',
+    content: '하하하 공부 열심히 해야되는데...ㅎㅎ',
+    author: {
+      id: '7879231e-12d9-4184-a9c5-7aacfae9dc12',
+      createdAt: '2023-02-12T14:16:41.386Z',
+      updateddAt: '2023-02-12T14:16:41.386Z',
+      email: 'test@test123.com',
+      username: '아무개',
+      thumbnailUrl: 'http://127.0.0.1:5000',
     },
-  },
-  join: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          message: '회원가입에 성공하였습니다.',
-        },
-      },
+    imgUrl: null,
+    id: '2db91d6b-47bd-4516-9089-7d5b58bd3271',
+    createdAt: '2023-02-16T11:44:54.702Z',
+    updateddAt: '2023-02-16T11:44:54.702Z',
+    favoriteCount: 0,
+    commentCount: 0,
+  }),
+  updateDiary: responseTemplate({
+    id: '2db91d6b-47bd-4516-9089-7d5b58bd3271',
+    createdAt: '2023-02-16T11:44:54.702Z',
+    updateddAt: '2023-02-16T11:47:52.121Z',
+    title: 'test',
+    content: '수정되었습니다.',
+    imgUrl: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    author: {
+      id: '7879231e-12d9-4184-a9c5-7aacfae9dc12',
+      createdAt: '2023-02-12T14:16:41.386Z',
+      updateddAt: '2023-02-12T14:16:41.386Z',
+      email: 'test@test123.com',
+      username: '아무개',
+      thumbnailUrl: 'http://127.0.0.1:5000',
     },
-  },
-  emailCheck: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          message: '사용가능한 이메일입니다.',
-        },
-      },
-    },
-  },
-  usernameCheck: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          message: '사용가능한 유저이름입니다.',
-        },
-      },
-    },
-  },
-  login: {
-    schema: {
-      example: {
-        success: true,
-        data: {
-          token:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0YWY4OGI2OC0yNzc4LTQ1NjEtOTMzZS0zZDg2YTFmNmJhODciLCJpYXQiOjE2NzQ5NzQzNzQsImV4cCI6MTY3NTA2MDc3NH0.7qoTypzUQTfIULMoKPLYpXOcpTVtQZcsMKjdSnHRT2U',
-          user: {
-            id: 'uuid',
-            createdAt: 'Data string',
-            updateddAt: 'Data string',
-            email: 'email string',
-            username: 'username string',
-            thumbnailUrl: 'url image path',
-            isAgree: true,
-            isAdmin: false,
-          },
-        },
-      },
-    },
-  },
+  }),
+  softDeleteDiary: responseTemplate({
+    message: '삭제되었습니다.',
+  }),
 };
--- src/diaries/diaries.controller.ts
+++ src/diaries/diaries.controller.ts
@@ -13,20 +13,40 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { FileUploadDto } from 'src/common/dto/FileUpload.dto';
 import { HttpApiExceptionFilter } from 'src/common/exceptions/http-api-exceptions.filter';
 import { multerOption } from 'src/common/utils/multer.options';
+import { responseExample } from 'src/constants/swagger';
 import { UserDTO } from 'src/users/dto/user.dto';
 import { JwtAuthGuard } from 'src/users/jwt/jwt.guard';
 import { DiariesService } from './diaries.service';
 import { DiaryFormDTO } from './dto/diary-form.dto';
 
+@ApiTags('Diary')
 @Controller('diaries')
 @UseFilters(HttpApiExceptionFilter)
 export class DiariesController {
   constructor(private readonly diariesService: DiariesService) {}
 
   @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'formdata instance에 append 시 key값을 image로 설정해주세요.',
+    type: FileUploadDto,
+  })
+  @ApiOperation({
+    summary: '일기 이미지 업로드',
+  })
+  @ApiCreatedResponse(responseExample.uploadUserImg)
   @UseInterceptors(FileInterceptor('image', multerOption('diaries')))
   uploadUserImg(@UploadedFile() file: Express.Multer.File) {
     console.log(file);
@@ -34,16 +54,29 @@ export class DiariesController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: '일기 전체 리스트 조회',
+  })
+  @ApiCreatedResponse(responseExample.getDiaries)
   getDiaries() {
     return this.diariesService.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: '일기 상세 조회',
+  })
+  @ApiCreatedResponse(responseExample.getDiary)
   getDiary(@Param('id', ParseUUIDPipe) id: string) {
     return this.diariesService.getOne(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: '일기 생성',
+  })
+  @ApiBearerAuth('access-token')
+  @ApiCreatedResponse(responseExample.createDiary)
   @UseGuards(JwtAuthGuard)
   createDiary(
     @Body() diaryFormDto: DiaryFormDTO,
@@ -53,6 +86,11 @@ export class DiariesController {
   }
 
   @Put(':id')
+  @ApiOperation({
+    summary: '일기 수정',
+  })
+  @ApiBearerAuth('access-token')
+  @ApiCreatedResponse(responseExample.updateDiary)
   @UseGuards(JwtAuthGuard)
   updateDiary(
     @Param('id', ParseUUIDPipe) id: string,
@@ -63,6 +101,11 @@ export class DiariesController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: '일기 삭제',
+  })
+  @ApiBearerAuth('access-token')
+  @ApiCreatedResponse(responseExample.softDeleteDiary)
   @UseGuards(JwtAuthGuard)
   deleteDiary(
     @Param('id', ParseUUIDPipe) id: string,
