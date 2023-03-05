ae7427162c2e64aeed9768abf00a5a9f16c16d15
yws1502 <yws1502@naver.com>
2023-03-05 17:41:31 +0900

    🚧 #19 - bookmark table에 user table 연결

--- src/bookmarks/bookmarks.entity.ts
+++ src/bookmarks/bookmarks.entity.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { CommonEntity } from 'src/common/entities/common.entity';
 import { DiaryEntity } from 'src/diaries/diaries.entity';
 import { UserEntity } from 'src/users/users.entity';
-import { Entity, Index } from 'typeorm';
+import { Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
 
 @Index('bookmarkId', ['id'], { unique: true })
 @Entity({
@@ -10,6 +10,13 @@ import { Entity, Index } from 'typeorm';
 })
 export class BookmarkEntity extends CommonEntity {
   @ApiProperty()
+  @ManyToOne(() => UserEntity, (author: UserEntity) => author.bookmarks, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({
+    name: 'author_id',
+    referencedColumnName: 'id',
+  })
   author: UserEntity;
 
   @ApiProperty()
