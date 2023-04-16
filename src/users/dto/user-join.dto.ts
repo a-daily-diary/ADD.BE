e8e1e51069fdf1b517bea5dad461f1a59b8ae59a
yws1502 <yws1502@naver.com>
2023-04-16 13:18:37 +0900

    🩹 #22 - 유저 정보 중 일부 속성 조회 제거

--- src/users/dto/user-join.dto.ts
+++ src/users/dto/user-join.dto.ts
@@ -1,16 +1,21 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
 import { UserEntity } from '../users.entity';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsBoolean } from 'class-validator';
+import { Column } from 'typeorm';
 
 export class UserJoinDTO extends PickType(UserEntity, [
   'email',
   'username',
-  'isAgree',
 ] as const) {
   @ApiProperty()
   @IsString()
   @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
   password: string;
+
+  @ApiProperty()
+  @IsBoolean()
+  @Column({ type: 'boolean', nullable: false })
+  isAgree: boolean;
 }
 
 export class UserEmailDTO extends PickType(UserEntity, ['email'] as const) {}
