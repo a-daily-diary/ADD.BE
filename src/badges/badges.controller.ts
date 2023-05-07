ec5a925ce2e6f4ccbd87fa6edda2df2a53d09f5b
yws1502 <yws1502@naver.com>
2023-05-07 16:06:12 +0900

    ✨ #55 - 뱃지 이미지 업로드 API 개발

--- src/badges/badges.controller.ts
+++ src/badges/badges.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { HttpApiExceptionFilter } from 'src/common/exceptions/http-api-exceptions.filter';
+import { BadgesService } from './badges.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
+@ApiTags('Badge')
 @Controller('badges')
-export class BadgesController {}
+@UseFilters(HttpApiExceptionFilter)
+export class BadgesController {
+  constructor(private readonly badgesService: BadgesService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  updateBadgeImg(@UploadedFile() file: Express.Multer.File) {
+    return this.badgesService.uploadImg(file);
+  }
+}
--- src/badges/badges.service.ts
+++ src/badges/badges.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AwsService } from 'src/aws.service';
 
 @Injectable()
-export class BadgesService {}
+export class BadgesService {
+  constructor(private readonly awsService: AwsService) {}
+
+  async uploadImg(file: Express.Multer.File) {
+    const uploadInfo = await this.awsService.uploadFileToS3('badges', file);
+    return { imgUrl: this.awsService.getAwsS3FileUrl(uploadInfo.key) };
+  }
+}
