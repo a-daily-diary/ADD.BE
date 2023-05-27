735e04bda7c0826c308681f16d70903c5679b9e5
yws1502 <yws1502@naver.com>
2023-05-27 23:26:38 +0900

    🚧 #63 - 약관 컨트롤러 등록

--- src/terms-agreements/terms-agreements.controller.ts
+++ src/terms-agreements/terms-agreements.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { TermsAgreementsService } from './terms-agreements.service';
+import { TermsAgreementFormDTO } from './dto/terms-agreemtn-form.dto';
 
 @Controller('terms-agreements')
-export class TermsAgreementsController {}
+export class TermsAgreementsController {
+  constructor(
+    private readonly termsAgreementsService: TermsAgreementsService,
+  ) {}
+
+  @Post()
+  createTermsAgreement(@Body() termsAgreementFormDTO: TermsAgreementFormDTO) {
+    return this.termsAgreementsService.createTermsAgreement(
+      termsAgreementFormDTO,
+    );
+  }
+}
--- src/terms-agreements/terms-agreements.module.ts
+++ src/terms-agreements/terms-agreements.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TermsAgreementsService } from './terms-agreements.service';
 import { TermsAgreementsController } from './terms-agreements.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TermsAgreementEntity } from './terms-agreements.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TermsAgreementEntity])],
   providers: [TermsAgreementsService],
   controllers: [TermsAgreementsController],
 })
