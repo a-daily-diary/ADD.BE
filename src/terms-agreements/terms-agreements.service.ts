cb546b089792852af0d54f9d61cd253dd3ab9abc
yws1502 <yws1502@naver.com>
2023-05-29 22:24:17 +0900

    🚧 #70 - 입력 받은 값들이 필수 값인지 확인하는 함수 생성

--- src/terms-agreements/terms-agreements.service.ts
+++ src/terms-agreements/terms-agreements.service.ts
@@ -16,6 +16,29 @@ export class TermsAgreementsService {
     return await this.termsAgreementRepository.findOneBy({ id });
   }
 
+  async getRequiredTermsAgreements() {
+    return await this.termsAgreementRepository.find({
+      where: { isRequired: true },
+    });
+  }
+
+  /**
+   * 파라미터로 전달받은 값이 필수 약관동의에 적합한지 확인해주는 함수
+   *
+   * validate한 경우 true 반환
+   * 그렇지 않은 경우 false 반환
+   */
+  async validatorRequiredTermsAgreements(checkTermsAgreementIdList: string[]) {
+    const requiredTermsAgreements = await this.getRequiredTermsAgreements();
+    const requiredTermsAgreementIdList = requiredTermsAgreements.map(
+      (el) => el.id,
+    );
+
+    return requiredTermsAgreementIdList.every((requiredId) =>
+      checkTermsAgreementIdList.includes(requiredId),
+    );
+  }
+
   async createTermsAgreement(termsAgreementFormDTO: TermsAgreementFormDTO) {
     const newTermsAgreement = this.termsAgreementRepository.create(
       termsAgreementFormDTO,
