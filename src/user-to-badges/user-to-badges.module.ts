b60eb145ecf2ad4339341339c42f38ac436e627c
yws1502 <yws1502@naver.com>
2023-06-04 23:32:16 +0900

    ✨ #65 - 뱃지 획득 처리 메소드 개발

--- src/user-to-badges/user-to-badges.module.ts
+++ src/user-to-badges/user-to-badges.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserToBadgesService } from './user-to-badges.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserToBadgeEntity } from './user-to-badges.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserToBadgeEntity])],
   providers: [UserToBadgesService],
 })
 export class UserToBadgesModule {}
--- src/user-to-badges/user-to-badges.service.ts
+++ src/user-to-badges/user-to-badges.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserToBadgeEntity } from './user-to-badges.entity';
+import { Repository } from 'typeorm';
+import { UserDTO } from 'src/users/dto/user.dto';
+import { BadgeEntity } from 'src/badges/badges.entity';
 
 @Injectable()
-export class UserToBadgesService {}
+export class UserToBadgesService {
+  constructor(
+    @InjectRepository(UserToBadgeEntity)
+    private readonly userToBadgeRepository: Repository<UserToBadgeEntity>,
+  ) {}
+
+  async saveUserToBadge(user: UserDTO, badge: BadgeEntity) {
+    const pinnedCount = await this.userToBadgeRepository
+      .createQueryBuilder('userToBadge')
+      .where('userToBadge.user = :user', { user })
+      .andWhere('userToBadge.isPinned = true')
+      .getCount();
+
+    const newUserToBadge = this.userToBadgeRepository.create({
+      user,
+      badge,
+      isPinned: pinnedCount < 8,
+    });
+    try {
+      await this.userToBadgeRepository.save(newUserToBadge);
+    } catch {
+      return false;
+    }
+
+    return true;
+  }
+}
