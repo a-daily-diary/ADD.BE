09f8a0feda96657714bac941f85a4d3d660aa7c7
yws1502 <yws1502@naver.com>
2023-06-05 18:29:47 +0900

    ✏️ #64 - 오타 수정

--- src/terms-agreements/dto/terms-agreement-form.dto.ts
+++ src/terms-agreements/dto/terms-agreement-form.dto.ts
@@ -2,6 +2,7 @@ import { PickType } from '@nestjs/swagger';
 import { TermsAgreementEntity } from '../terms-agreements.entity';
 
 export class TermsAgreementFormDTO extends PickType(TermsAgreementEntity, [
+  'id',
   'title',
   'content',
   'isRequired',
