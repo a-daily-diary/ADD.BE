01f2f87145b243926053e91416f44dc94aa1e5ab
yws1502 <yws1502@naver.com>
2023-06-06 10:05:37 +0900

    ✨ #65 - 북마크 10회 등록 시 뱃지 획득 로직 추가

--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -6,6 +6,9 @@ import { DiariesService } from 'src/diaries/diaries.service';
 import { UserDTO } from 'src/users/dto/user.dto';
 import { Repository } from 'typeorm';
 import { BookmarkEntity } from './bookmarks.entity';
+import { BadgeEntity } from 'src/badges/badges.entity';
+import { BadgesService } from 'src/badges/badges.service';
+import { BadgeCode } from 'src/types/badges.type';
 
 @Injectable()
 export class BookmarksService {
@@ -13,6 +16,7 @@ export class BookmarksService {
     @InjectRepository(BookmarkEntity)
     private readonly bookmarkRepository: Repository<BookmarkEntity>,
     private readonly diariesSerivce: DiariesService,
+    private readonly badgesService: BadgesService,
   ) {}
 
   async findBookmarkByUserAndDiary(user: UserDTO, diary: DiaryEntity) {
@@ -42,7 +46,21 @@ export class BookmarksService {
 
     await this.bookmarkRepository.save(newBookmark);
 
-    return { message: '북마크가 등록되었습니다.' };
+    const registerBookmarkCount = await this.bookmarkRepository
+      .createQueryBuilder('bookmark')
+      .leftJoin('bookmark.user', 'user')
+      .where('user.id = :userId', { userId: user.id })
+      .getCount();
+
+    let badgeToGet: BadgeEntity;
+
+    // FIXME: 이미 획득한 경우 예외 처리
+    // FIXME: 획득 조건 상수 혹은 함수로 분리
+    if (registerBookmarkCount === 10) {
+      badgeToGet = await this.badgesService.findById(BadgeCode.bookmark);
+    }
+
+    return { message: '북마크가 등록되었습니다.', badge: badgeToGet || null };
   }
 
   async unregister(diaryId: string, user: UserDTO) {
