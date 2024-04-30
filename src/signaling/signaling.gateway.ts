5a4da41f0e118b168715cd0187d3d4a7d9764ca7
yws1502 <yws1502@naver.com>
2024-04-30 15:08:45 +0900

    ✨ #102 - Random Matching 기능 추가

--- src/signaling/signaling.gateway.ts
+++ src/signaling/signaling.gateway.ts
@@ -21,34 +21,73 @@ export class SignalingGateway
   @WebSocketServer()
   namespace: Namespace;
 
-  // FIXME: matchingWaitingUserObject에서 BlackList를 관리할지 아니면 user의 id로 DB에 직접 조회할지 고민 필요
-  private matchingWaitingUserObject: Record<string, MatchingWaitingUser> = {};
+  // key = socket id, value = user information
+  private matchingQueue: Record<string, MatchingWaitingUser> = {};
 
-  private WAITING_ROOM = 'WAITING_ROOM';
+  private matchedSocketIds: string[] = [];
 
   afterInit() {
     // socket 서버가 구동되었을 때 5초에 한번씩 랜덤 매칭 로직이 수행 됨.
     console.log('Start random matching job.');
+
+    setInterval(() => {
+      const matchingQueueSocketIds = Object.keys(this.matchingQueue);
+
+      if (matchingQueueSocketIds.length === 0) return;
+
+      for (let i = 0; i < matchingQueueSocketIds.length; i++) {
+        if (matchingQueueSocketIds.length === this.matchedSocketIds.length)
+          break;
+
+        const offerSocketId = matchingQueueSocketIds[i];
+
+        if (this.matchedSocketIds.includes(offerSocketId)) continue;
+
+        // TODO: BlackList 목록 필터 조건 추가 예정
+        const filteredSocketIds = matchingQueueSocketIds.filter(
+          (waitingUserSocketId) =>
+            waitingUserSocketId !== offerSocketId &&
+            this.matchedSocketIds.includes(waitingUserSocketId) === false,
+        );
+
+        if (filteredSocketIds.length === 0) continue;
+
+        const randomIndex = Math.floor(
+          Math.random() * filteredSocketIds.length,
+        );
+
+        const answerSocketId = filteredSocketIds[randomIndex];
+
+        this.matchedSocketIds.push(offerSocketId);
+        this.matchedSocketIds.push(answerSocketId);
+
+        // TODO: Signaling 작업 수행
+      }
+
+      this.matchedSocketIds.forEach((matchedSocketId) => {
+        delete this.matchingQueue[matchedSocketId];
+      });
+
+      this.matchedSocketIds = [];
+    }, 5000);
   }
 
   handleConnection(socket: Socket) {
-    socket.join(this.WAITING_ROOM);
-
     socket.on('userInfo', (userInfo: MatchingWaitingUser) => {
       console.log(
         `Connection username is ${userInfo.username} (socket id: ${socket.id})`,
       );
-      this.matchingWaitingUserObject[socket.id] = userInfo;
+      this.matchingQueue[socket.id] = userInfo;
     });
   }
 
   handleDisconnect(socket: Socket) {
     console.log(
       `Disconnect username is ${
-        this.matchingWaitingUserObject[socket.id].username
+        this.matchingQueue[socket.id]?.username
       } (socket id: ${socket.id})`,
     );
-    delete this.matchingWaitingUserObject[socket.id];
+    delete this.matchingQueue[socket.id];
   }
 
   @SubscribeMessage('message')
--- src/signaling/signaling.type.ts
+++ src/signaling/signaling.type.ts
@@ -1,4 +1,5 @@
 export interface MatchingWaitingUser {
   id: string;
   username: string;
+  // blackList: string[]; userId List // TODO: 차후 blacklist 기능 구현 시 추가 예정
 }
