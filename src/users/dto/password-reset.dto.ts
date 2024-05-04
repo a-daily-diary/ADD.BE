dc1448b493ff0159628fbb5cd76b0ed7370db001
yws1502 <yws1502@naver.com>
2024-05-04 11:48:34 +0900

    fix: passwordResetDTO에서 tempToken 컬럼 추가 #119

--- src/users/dto/password-reset.dto.ts
+++ src/users/dto/password-reset.dto.ts
@@ -1,13 +1,16 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Column } from 'typeorm';
 import { UserEntity } from '../users.entity';
-import { IsNotEmpty, IsString } from 'class-validator';
 
-export class PasswordResetDTO extends PickType(UserEntity, [
-  'email',
-  'tempToken',
-] as const) {
+export class PasswordResetDTO extends PickType(UserEntity, ['email'] as const) {
   @ApiProperty()
   @IsString()
   @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
   password: string;
+
+  @ApiProperty()
+  @IsUUID()
+  @Column({ type: 'uuid', nullable: true, default: null })
+  tempToken: string | null;
 }
