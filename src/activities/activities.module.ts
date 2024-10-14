2ef71ff2d22ee13532ab083a2095e32ad3e05138
yws1502 <yws1502@naver.com>
2024-10-14 21:34:20 +0900

    ✨ #129 - 활동 그래프 API에 매칭 이력 추가

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -4,9 +4,16 @@ import { ActivitiesController } from './activities.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiaryEntity } from 'src/diaries/diaries.entity';
 import { CommentEntity } from 'src/comments/comments.entity';
+import { MatchingHistoryEntity } from 'src/matching-histories/matching-histories.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DiaryEntity, CommentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      DiaryEntity,
+      CommentEntity,
+      MatchingHistoryEntity,
+    ]),
+  ],
   providers: [ActivitiesService],
   controllers: [ActivitiesController],
   exports: [ActivitiesService],
